a1fe34424a1cdbc5e084cd5593c722fd7a81a066
Beomjae Lee <beomjae@gmail.com>
2017-07-16 17:40:32 +0900

    Add Event

--- js/app.js
+++ js/app.js
@@ -34,16 +34,33 @@
 
 $(document).ready(function() {
   var $heart = $(".heart");
+  var $comment = $(".comment");
+  var $profileIcon = $(".profile__icon");
+  var $profileOverlayCloseButton = $(".profileOverlay i");
+  var $profileOverlayCancelMenu = $("#profileOverlay-cancel");
+
   $heart.click(function(){
     $(this).toggleClass("fa-heart-o");
     $(this).toggleClass("fa-heart heart-click");
   });
 
-  var $comment = $(".comment");
   $comment.click(function(){
-    console.log("clicked");
     $(this).toggleClass("fa-comment-o");
     $(this).toggleClass("fa-comment comment-click");
   });
+
+  $profileIcon.click(function(){
+    $(".profileOverlay").fadeIn();
+  });
+
+  $profileOverlayCloseButton.click(function() {
+    $(".profileOverlay").fadeOut();
+  });
+
+  $profileOverlayCancelMenu.click(function(){
+    $(".profileOverlay").fadeOut();
+  });
+
+
 
 });
